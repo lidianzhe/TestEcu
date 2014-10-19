00659a4ef6f92226d139299e5f7fa2839c0af782
lihongjun <lihongjun@aizhetech.com>
2014-10-20 00:24:27 +0800

    change relay to 0.5

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -28,7 +28,7 @@ void MainWindow::on_pushButton_Open_clicked()
     if(ret==EMA_SUCCESS)
         ui->plainTextEdit_Log->appendPlainText("open success!");
     else if(ret==EMA_ERROR_ALREADY_OPENED)
-        ui->plainTextEdit_Log->appendPlainText("EMA_ERROR_DEVICE_OPENED");
+        ui->plainTextEdit_Log->appendPlainText("EMA_ERROR_ALREADY_OPENED");
     else
         ui->plainTextEdit_Log->appendPlainText("open fail");
 }
@@ -37,7 +37,7 @@ void MainWindow::on_btnRelay_clicked()
 {
     m_emaEvent.cbSize = sizeof(EMA_EVENT);
     m_emaEvent.eventType = EMA_EVENT_TYPE_RELAY_WRITE;
-    m_emaEvent.relayInterval=10;
+    m_emaEvent.relayInterval=5;
     m_emaEvent.relayValue = EMA_EVENT_VALUE_RELAY_ABNORMAL;
     int ret =ema_writeEvent(m_emaHandle,&m_emaEvent);
     if(ret==EMA_SUCCESS)
